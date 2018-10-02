06c3d039f696147d1c115ff64991d36a015d4f07
Joseph Chalupa <grammerking113@gmail.com>
2018-10-02 10:59:57 -0500

    Project

---
+++ 26/26/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	cout << fixed << setprecision(2);
+
+	//declare array
+	double prices[10] = { 10.5, 25.5, 9.75, 6.0, 35.0, 100.4, 10.65, .56, 14.75, 4.78 };
+	//declare variable
+	double increase = 0.0;
+	double increaseNum;
+
+	//update prices
+	cout << "Enter increase percentage (for example, enter 15 for 15%): ";
+	cin >> increase;
+	increaseNum = increase * .01;
+
+	//end for
+	for (int i = 0; i < size(prices); i++)
+	{
+		prices[i] = prices[i] + (prices[i] * increaseNum);
+		cout << prices[i] << endl;
+	}
+	//display contents of array
+	
+
+	//end for
+
+	system("pause");
+	return 0;
+}	//end of main function
---
+++ Number23/Number23/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int points[5] = { 62, 75, 100, 83, 85 };
+	char grades[5] = { 'D', 'C', 'A', 'B', 'B' };
+
+	int x = 0;
+
+		cout << points[0] << "->" << grades[0] << endl;
+		cout << points[1] << "->" << grades[1] << endl;
+		cout << points[2] << "->" << grades[2] << endl;
+		cout << points[3] << "->" << grades[3] << endl;
+		cout << points[4] << "->" << grades[4] << endl;
+		x += 1;
+	
+
+
+	system("pause");
+	return 0;
+}	//end of main function
